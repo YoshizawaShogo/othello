711550478631bab3e262ccb7f7f384d93cbf1ecf
Shogo Yoshizawa <shogo.yoshizawa.2000@gmail.com>
2026-03-07 13:05:48 +0900

    hello world with eframe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
+
+    eframe::run_simple_native("Hello egui", options, |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello world");
+        });
+    })
 }
